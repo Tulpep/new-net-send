5fe74ee290484fe841a02b930559bc3e801ddc2b
Ricardo Polo <ricardo.polo@tulpep.com>
2014-04-14 16:28:06 -0500

    Send argumnets to original NET

--- Tulpep.CentralNetSend.CommandLine/Program.cs
+++ Tulpep.CentralNetSend.CommandLine/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CentralNetSend
 {
@@ -10,6 +8,45 @@ namespace CentralNetSend
     {
         static void Main(string[] args)
         {
+            if(args.Length != 0)
+            {
+                if(args[0].ToLower() == "send" && args.Count() == 3)
+                {
+                    string computerName = args[1];
+                    string message = args[2];
+
+                    //MAndar mensaje al servidor
+                }
+                else
+                {
+                    var process = new Process();
+                    process.StartInfo.FileName = "net.exe";
+                    process.StartInfo.Arguments = String.Join(" ", args);
+
+                    // set up output redirection
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.UseShellExecute = false;
+                    process.EnableRaisingEvents = true;
+
+                    // see below for output handler
+                    process.ErrorDataReceived += ShowInfoOfNet;
+                    process.OutputDataReceived += ShowInfoOfNet;
+
+                    process.Start();
+
+                    process.BeginErrorReadLine();
+                    process.BeginOutputReadLine();
+
+                    process.WaitForExit();
+                }
+            }
+        }
+
+        static void ShowInfoOfNet(object sender, DataReceivedEventArgs e)
+        {
+            Console.Write(e.Data + Environment.NewLine);
         }
     }
 }
