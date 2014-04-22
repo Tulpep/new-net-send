d9b7c8cc1b60dec5ae8dd028a71c25b7f0bb4383
Ricardo Polo <ricardo.polo@tulpep.com>
2014-04-22 12:09:51 -0500

    Net send service with automatic installer

--- Tulpep.CentralNetSend.WebApiService/Program.cs
+++ Tulpep.CentralNetSend.WebApiService/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -12,14 +14,169 @@ namespace Tulpep.CentralNetSend.WebApiService
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        /// 
+
+
+        const string serviceName = "CentralNetSend";
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-            { 
-                new CentralNetSend() 
-            };
-            ServiceBase.Run(ServicesToRun);
+            if (args.Length == 0)
+            {
+                // Run your service normally.
+                ServiceBase[] ServicesToRun = new ServiceBase[] { new CentralNetSend() };
+                ServiceBase.Run(ServicesToRun);
+            }
+            else if (args.Length == 1)
+            {
+                switch (args[0])
+                {
+                    case "-install":
+                        InstallService();
+                        StartService();
+                        break;
+                    case "-uninstall":
+                        StopService();
+                        UninstallService();
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        private static bool IsInstalled()
+        {
+            using (ServiceController controller =
+                new ServiceController(serviceName))
+            {
+                try
+                {
+                    ServiceControllerStatus status = controller.Status;
+                }
+                catch
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        private static bool IsRunning()
+        {
+            using (ServiceController controller =
+                new ServiceController(serviceName))
+            {
+                if (!IsInstalled()) return false;
+                return (controller.Status == ServiceControllerStatus.Running);
+            }
+        }
+
+        private static AssemblyInstaller GetInstaller()
+        {
+            AssemblyInstaller installer = new AssemblyInstaller(
+                typeof(ProjectInstaller).Assembly, null);
+            installer.UseNewContext = true;
+            return installer;
+        }
+
+        private static void InstallService()
+        {
+            if (IsInstalled()) return;
+
+            try
+            {
+                using (AssemblyInstaller installer = GetInstaller())
+                {
+                    IDictionary state = new Hashtable();
+                    try
+                    {
+                        installer.Install(state);
+                        installer.Commit(state);
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            installer.Rollback(state);
+                        }
+                        catch { }
+                        throw;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void UninstallService()
+        {
+            if (!IsInstalled()) return;
+            try
+            {
+                using (AssemblyInstaller installer = GetInstaller())
+                {
+                    IDictionary state = new Hashtable();
+                    try
+                    {
+                        installer.Uninstall(state);
+                    }
+                    catch
+                    {
+                        throw;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void StartService()
+        {
+            if (!IsInstalled()) return;
+
+            using (ServiceController controller =
+                new ServiceController(serviceName))
+            {
+                try
+                {
+                    if (controller.Status != ServiceControllerStatus.Running)
+                    {
+                        controller.Start();
+                        controller.WaitForStatus(ServiceControllerStatus.Running,
+                            TimeSpan.FromSeconds(10));
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static void StopService()
+        {
+            if (!IsInstalled()) return;
+            using (ServiceController controller =
+                new ServiceController(serviceName))
+            {
+                try
+                {
+                    if (controller.Status != ServiceControllerStatus.Stopped)
+                    {
+                        controller.Stop();
+                        controller.WaitForStatus(ServiceControllerStatus.Stopped,
+                             TimeSpan.FromSeconds(10));
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
         }
     }
 }
