8ae6f578450b2fba7347327698b31e71e8cf2b1b
Ricardo Polo <ricardo.polo@tulpep.com>
2014-04-22 11:55:46 -0500

    Reading URL of Server from registry in service

--- Tulpep.CentralNetSend.WebApiService/Service1.cs
+++ Tulpep.CentralNetSend.WebApiService/Service1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Owin.Hosting;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,8 +21,18 @@ namespace Tulpep.CentralNetSend.WebApiService
 
         protected override void OnStart(string[] args)
         {
-            string baseAddress = "http://localhost:56890/";
-            WebApp.Start<Startup>(url: baseAddress);
+            string urlOfServer;
+
+            using (RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                using (RegistryKey centralNetSendKey = hklm.OpenSubKey(@"SOFTWARE\Tulpep\CentralNetSend"))
+                {
+                    urlOfServer = centralNetSendKey.GetValue("APIServer").ToString();
+                }
+
+            }
+
+            WebApp.Start<Startup>(url: urlOfServer);
         }
 
         protected override void OnStop()
